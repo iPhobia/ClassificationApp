28b595cd9221ab101384bbba5d527ab2e3d14662
Phobia <pashaphb@gmail.com>
2018-06-03 17:20:21 +0300

    cmd run works

--- ClassificationApp/MainWindow.xaml.cs
+++ ClassificationApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Diagnostics;
+using System.IO;
 
 namespace ClassificationApp
 {
@@ -17,6 +20,31 @@ namespace ClassificationApp
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public void OnButtonClicked(object sender, RoutedEventArgs args)
+        {
+            const string commands = @"matlab.exe";
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false
+
+                }
+            };
+            process.Start();
+
+            using (StreamWriter pWriter = process.StandardInput)
+            {
+                if (pWriter.BaseStream.CanWrite)
+                {
+                    foreach (var line in commands.Split('\n'))
+                        pWriter.WriteLine(line);
+                }
+            }
         }
     }
 }
